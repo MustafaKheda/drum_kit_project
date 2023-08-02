6dfc76bd3371ca8c6f55f6708d52ae59d576fd66
Mustafa <mustafa.kheda@kadellabs.com>
2023-08-02 10:03:37 +0530

    Version 2

--- src/components/Drum.js
+++ src/components/Drum.js
@@ -1,16 +1,44 @@
 import React, { useEffect } from "react";
 import DrumPad from "./DrumPad";
-import tom from "../assets/audio/stomp.mp3";
-import kick from "../assets/audio/kickDrum.mp3";
+import tom from "../assets/audio/Small-Tom-Drum-Hit-Level-4A.mp3";
+import kick from "../assets/audio/Kick-Drum.mp3";
+import cymbalCrash from "../assets/audio/China-Cymbal-Crash-Level-6A.mp3";
+import cymbalSideB from "../assets/audio/China-Cymbal-Slide-Scrape-B.mp3";
+import cymbalSideC from "../assets/audio/China-Cymbal-Slide-Scrape-C.mp3";
+import cymbalSideD from "../assets/audio/China-Cymbal-Slide-Scrape-D.mp3";
+import cymbalHitA from "../assets/audio/Crash-Cymbal-Hit-A.mp3";
+import cymbalHitC from "../assets/audio/Crash-Cymbal-Hit-C.mp3";
+import floorTom from "../assets/audio/Floor-Tom-Drum-Hit-Level-6B.mp3";
+import hiHatA2 from "../assets/audio/Hi-Hat-Closed-Hit-A2.mp3";
+import hiHatE1 from "../assets/audio/Hi-Hat-Closed-Hit-E1.mp3";
+import hiHatFoot from "../assets/audio/Hi-Hat-Foot-Pedal-Close-B.mp3";
+import highTom from "../assets/audio/high-tom_F_minor.wav";
+import mediumTom from "../assets/audio/Medium-Tom-Drum-Hit-Level-7A.mp3";
+import smallTom from "../assets/audio/Small-Tom-Drum-Hit-Level-4A.mp3";
+import snareDrum from "../assets/audio/Snare-Drum-Hit-Level-6a.mp3";
+import SplashCymbal from "../assets/audio/Splash-Cymbal-Hit-C.mp3";
+
 import { Howl } from "howler";
 import { Button } from "@mui/material";
 function Drum() {
   const drumPads = [
-    { name: "Tom", key: "Q", soundSrc: tom },
-    { name: "Kick", key: "W", soundSrc: kick },
-    { name: "E", key: "E", soundSrc: tom },
-    { name: "R", key: "R", soundSrc: kick },
-    { name: "H", key: "H", soundSrc: tom },
+    { name: "cymbalCrash", key: "Q", soundSrc: cymbalCrash },
+    { name: "cymbalSideB", key: "W", soundSrc: cymbalSideB },
+    { name: "cymbalSideC", key: "E", soundSrc: cymbalSideC },
+    { name: "cymbalSideD", key: "R", soundSrc: cymbalSideD },
+    { name: "cymbalHitA", key: "T", soundSrc: cymbalHitA },
+    { name: "cymbalHitC", key: "Y", soundSrc: cymbalHitC },
+    { name: "SplashCymbal", key: "U", soundSrc: SplashCymbal },
+    { name: "hiHatA2", key: "K", soundSrc: hiHatA2 },
+    { name: "hiHatE1", key: "L", soundSrc: hiHatE1 },
+    { name: "hiHatFoot", key: "J", soundSrc: hiHatFoot },
+    { name: "highTom", key: "G", soundSrc: highTom },
+    { name: "mediumTom", key: "H", soundSrc: mediumTom },
+    { name: "smallTom", key: "D", soundSrc: smallTom },
+    { name: "Tom", key: "A", soundSrc: tom },
+    { name: "floorTom", key: "F", soundSrc: floorTom },
+    { name: "snareDrum", key: "S", soundSrc: snareDrum },
+    { name: "Kick", key: "B", soundSrc: kick },
   ];
 
   const playSound = (soundSrc) => {
@@ -46,6 +74,7 @@ function Drum() {
 
   return (
     <div className="drum-kit">
+      <div className=""></div>
       {drumPads.map((drumPad, index) => {
         const { key, soundSrc, name } = drumPad;
         return (
@@ -55,15 +84,10 @@ function Drum() {
             onClick={() => playSound(soundSrc)}
             // onKeyPress={(e) => onKeyPlaySound(e)}
           >
-            {name}
+            {name} {key}
           </Button>
         );
       })}
-      {/* <DrumPad
-        key={drumPad.key}
-        keyTrigger={drumPad.key}
-        soundSrc={drumPad.soundSrc}
-      /> */}
     </div>
   );
 }
